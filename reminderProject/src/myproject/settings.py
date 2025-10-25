dce0062d8c7d38e4ec43fbc91fb6ff7efcf310ff
AndrewTruong <takeiteasy@andrewMac.local>
2025-10-25 11:02:52 -0700

    add to setting.py in myproject

--- reminderProject/src/myproject/settings.py
+++ reminderProject/src/myproject/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # our apps
+    'chatbox',
+    'accounts',
+    'task',
 ]
 
 MIDDLEWARE = [
