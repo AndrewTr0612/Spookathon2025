97e4a90d263247a2da585a5d0c94d5c2e091c11f
AndrewTruong <takeiteasy@andrewMac.local>
2025-10-25 12:16:37 -0700

    installed posgrestsql

--- reminderProject/src/chatbox/models.py
+++ reminderProject/src/chatbox/models.py
@@ -1,16 +1,3 @@
 from django.db import models
-from django.conf import settings
 
 # Create your models here.
-
-class Message(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='messages')
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    is_read = models.BooleanField(default=False)
-    
-    class Meta:
-        ordering = ['-created_at']
-    
-    def __str__(self):
-        return f"{self.user.username}: {self.content[:50]}"
