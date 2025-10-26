f803b0427d8d52d5509ae724a76d778d6e755849
ThienAnTruong(Andrew) <166311237+AndrewTr0612@users.noreply.github.com>
2025-10-26 14:21:46 -0700

    Delete Spookaminder directory

--- Spookaminder/src/miniGame/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.game_home, name='game_home'),
-    path('buy-water/', views.buy_water, name='buy_water'),
-    path('water-plant/', views.water_plant, name='water_plant'),
-    path('stats/', views.game_stats, name='game_stats'),
-]
